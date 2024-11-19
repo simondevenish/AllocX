be6f8161d4e919381f02dec30fd4cee0121099d3
Simon Jordan <simon@thepolygonloft.com>
2024-11-19 13:57:06 +0000

    Basic allocx.* Implementation

---
+++ include/allocx/allocx.h
@@ -0,0 +1,6 @@
+#ifndef ALLOCX_H
+#define ALLOCX_H
+
+#include "allocx_core_types.h"
+
+#endif // ALLOCX_H
---
+++ src/allocx.c
@@ -0,0 +1,7 @@
+
+#include "allocx.h" // Core type definitions
+
+#include <stdlib.h>  // For malloc, free
+#include <stdio.h>   // For debug output
+#include <string.h>  // For memset, memcpy
+#include <assert.h>  // For assertions
